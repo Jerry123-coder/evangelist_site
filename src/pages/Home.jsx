cdc0a6d938eb4d8b98ff688a6dfd25e61ca77f6e
Just-Eto <60091053+Just-Eto@users.noreply.github.com>
2024-05-24 16:33:08 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -290,9 +290,12 @@ const Home = () => {
                 </div>
                 <div>
                   {" "}
+                <a href="https://bible.usccb.org/daily-bible-reading" target="_blank" rel="noopener noreferrer">
                   <div className="text-xl lg:text-4xl font-bold">
                     Daily Reading
                   </div>
+                </a>
+
                   <div className="text-sm lg:text-xl">
                     Access and meditate on the readings for today{" "}
                   </div>
