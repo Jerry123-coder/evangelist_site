7c2fe5a0145b2026bda426bab47d3549af9edfb7
freddiequinson <144603231+freddiequinson@users.noreply.github.com>
2024-12-11 15:54:15 +0000

    Updated ministries page

--- src/data/ministries.js
+++ src/data/ministries.js
@@ -30,8 +30,8 @@ export const ministries = [
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore officia iste maxime! Dolorem perferendis odio corporis voluptatum corrupti, molestiae fugiat pariatur similique expedita, quas obcaecati ab at, aut non sunt?",
   },
   {
-    name: "Extraordinary ministers of holy Eucharist",
+    name: "Extraordinary ministers of Holy Eucharist",
     details:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore officia iste maxime! Dolorem perferendis odio corporis voluptatum corrupti, molestiae fugiat pariatur similique expedita, quas obcaecati ab at, aut non sunt?",
   },
-];
+].sort((a, b) => a.name.localeCompare(b.name));
