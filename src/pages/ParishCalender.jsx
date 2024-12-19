726373c76c8bf3bf7b42f17313377cd8a423e7e8
Just-Eto <60091053+Just-Eto@users.noreply.github.com>
2024-12-19 08:29:29 +0000

    Sacrament Page

--- src/pages/ParishCalender.jsx
+++ src/pages/ParishCalender.jsx
@@ -45,16 +45,16 @@ const DetailedSacrament = ({ title, description, image, reversed = false }) => (
   <div
     className={`flex flex-col ${
       reversed ? "md:flex-row-reverse" : "md:flex-row"
-    } items-center justify-between p-4 md:p-8 bg-white`}
+    } items-center justify-between md:pt-[50px] bg-white`}
   >
-    <div className="w-full md:w-1/2 px-4 md:px-8 mb-6 md:mb-0">
-      <p className="text-sm text-gray-600 uppercase mb-2">SACRAMENTS</p>
-      <h2 className="text-2xl md:text-3xl font-bold text-blue-900 mb-4">
+    <div className="w-full md:w-7/12 px-4 md:px-20 mb-6 md:mb-0">
+      <p className="text-sm text-gray-600 uppercase sm:mt-10 md:mt-0  md:mb-2">SACRAMENTS</p>
+      <h2 className="text-2xl md:text-3xl mt- font-bold text-blue-900 mb-4">
         {title}
       </h2>
       <p className="text-sm md:text-base text-gray-700">{description}</p>
     </div>
-    <img src={image} alt={title} className=" md:w-1/2 rounded-lg" />
+    <img src={image} alt={title} className="md:h-[30rem] w-full md:w-5/12   " />
   </div>
 );
 
@@ -118,23 +118,53 @@ const SacramentsGrid = () => (
 
 const detailedSacraments = [
   {
-    title: "Anointing of The Sick",
+    title: "Baptism",
     description:
-      "Formerly known as Last Rites or Extreme Unction, the Sacrament of Anointing of the Sick is a ritual of healing appropriate for physical, mental, and spiritual illness. This sacrament offers the grace of the Holy Spirit to strengthen, comfort, and heal the sick.",
+      "For Catholics, the Sacrament of Baptism is the gateway to the Christian life. It is the first of the three Sacraments of Initiation, followed by Confirmation and the Eucharist. Baptism cleanses us from original sin, makes us members of Christ and His Church, and provides us with the grace to live a life of faith. Whether administered to infants or adults, Baptism represents the Church's celebration of God’s embrace and the beginning of a lifelong journey of faith.",
     image: "/2.jpeg",
-  },
-  {
-    title: "Holy Orders",
-    description:
-      "The Sacrament of Holy Orders is the means by which men are ordained as deacons, priests, or bishops. This sacrament confers the grace and spiritual power necessary to perform the sacred duties of the clergy.",
-    image: "/4.jpeg",
     reversed: true,
   },
   {
     title: "Holy Eucharist",
     description:
-      "The Eucharist, or Holy Communion, is the source and summit of the Christian life. Catholics believe that during the Mass, the bread and wine become the actual Body and Blood of Jesus Christ through the process of transubstantiation.",
+      "The Eucharist, or Holy Communion, is the source and summit of the Christian life. Catholics believe that during the Mass, the bread and wine become the actual Body and Blood of Jesus Christ through the process of transubstantiation. Participating in the Eucharist allows the faithful to receive Jesus Christ into their hearts and souls, providing spiritual nourishment and strengthening their bond with God. The Eucharist is a sacrificial meal, commemorating Jesus' Last Supper and His sacrifice on the cross.",
+    image: "/4.jpeg",
+  },
+  {
+    title: "Confirmation",
+    description:
+      "Confirmation is the Sacrament of mature Christian commitment and a deepening of baptismal gifts. It is the second of the three Sacraments of Initiation and is typically received by adolescents, though adults can also be confirmed. Through Confirmation, the Holy Spirit bestows His gifts, strengthening the recipient to live out their faith with courage and conviction. It is a call to witness and proclaim the Gospel in word and deed.",
     image: "/3.jpeg",
+    reversed: true,
+  },
+
+  // {
+  //   title: "Reconciliation",
+  //   description:
+  //     "Also known as Confession or Penance, the Sacrament of Reconciliation is a profound expression of God's mercy. This sacrament involves three parts: conversion, confession, and celebration. By confessing our sins to a priest and performing an act of penance, we receive God's forgiveness and are reconciled with Him and the Church. This sacrament reminds us of God's unconditional love and calls us to forgive others as we have been forgiven.",
+  //   image: "/5.jpeg",
+  // },
+  {
+    title: "Anointing of The Sick",
+    description:
+      "Formerly known as Last Rites or Extreme Unction, the Sacrament of Anointing of the Sick is a ritual of healing appropriate for physical, mental, and spiritual illness. This sacrament offers the grace of the Holy Spirit to strengthen, comfort, and heal the sick. It can be received more than once, especially in cases of serious illness or impending surgery. The Anointing of the Sick brings the comforting presence of Christ to those in need.",
+    image: "/6.jpeg",
+    // reversed: true,
+  },
+  {
+    title: "Holy Orders",
+    description:
+      "The Sacrament of Holy Orders is the means by which men are ordained as deacons, priests, or bishops. This sacrament confers the grace and spiritual power necessary to perform the sacred duties of the clergy. Those who receive Holy Orders are called to lead the Church, administer the sacraments, and proclaim the Gospel. It is a lifelong commitment to serve God and His people, guiding them on their spiritual journey.",
+    image: "/7.jpeg",
+    reversed: true,
+  },
+
+  {
+    title: "Matrimony",
+    description:
+      "The Sacrament of Marriage, or Holy Matrimony, is a covenant by which a man and a woman establish a partnership for their whole life. This sacrament reflects the union of Christ and His Church and is a public declaration of love and commitment. Through marriage, couples are called to reflect God's love in their relationship, support each other in faith, and raise their children in the teachings of the Church. Marriage is a vocation of self-giving love and service.",
+    image: "/8.jpeg",
+    // reversed: true,
   },
 ];
 
@@ -153,6 +183,7 @@ const ParishCalender = () => (
         />
       )
     )}
+    <div className="mt-20"></div>
   </div>
 );
 
