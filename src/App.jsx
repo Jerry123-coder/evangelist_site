db86cc5747e9b6e6ee5fbba6b66c157448dd7712
Jerry123-coder <144603231+freddiequinson@users.noreply.github.com>
2025-03-12 17:40:28 +0000

    Fix page scrolling issue by adding ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes/Router";
 import Footer from "./components/Footer.jsx";
 import WhatsAppButton from "./components/WhatsApp.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 // import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       {/* <Navbar /> */}
       <main>
         {" "}
